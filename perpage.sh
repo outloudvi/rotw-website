2b65d704011d38ee690f8db5da668e5914529140
OutloudVI <outloudvi@outlook.com>
2017-03-16 23:35:47 +0800

    Fix (really)

--- perpage.sh
+++ perpage.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 cd $1
-dest='./index.htm'
+dest='index.html'
 
 echo '[AddItem] Indexing $1...'
 #base-generate.sh $1
-cd $1
-rm index.htm
+rm index.html
 echo '<!DOCTYPE HTML><meta charset=utf-8><title>页面列表</title><ul>' >> $dest
 for i in $(ls . | sed 's/.html//')
 do
-	echo '<li><a href="$i.html">$i</a></li>' >> $dest
+	echo '<li><a href="'$i'.html">'$i'</a></li>' >> $dest
 done
 echo '</ul>' >> $dest
