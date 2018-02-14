7ee517f38da14321c5ffae86a7004b27763c9e4d
Outvi V <19144373+outloudvi@users.noreply.github.com>
2018-02-14 12:41:34 +0800

    Fix quote problem

--- assets/httpsrec.js
+++ assets/httpsrec.js
@@ -6,7 +6,7 @@ x = "rotw@surge 被嵌入。请注意您的浏览安全。"
 }
 
 if ( window.location.protocol == "https://"){
-  x = "您在使用 HTTP 协议访问我们的站点。建议<a href="https://rotw.surge.sh">使用 HTTPS 协议</a>，更安全。"
+  x = "您在使用 HTTP 协议访问我们的站点。建议<a href='https://rotw.surge.sh'>使用 HTTPS 协议</a>，更安全。"
 }
 
 if (x != "") {
